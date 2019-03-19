2f60d9740360cbb8d2f1314ef32d9649c0b3ba1a
heymanth <c.hemanth92@gmail.com>
2019-03-20 00:30:58 +0530

    update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,11 @@
-package com.mycompany.app;
-
-/**
- * Hello world!
- */
-public class App
-{
-
-    private final String message = "Hello World!";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
-
-    private final String getMessage() {
-        return message;
-    }
-
-}
+/* This is a simple Java program. 
+FileName : "HelloWorld.java". */
+class HelloWorld 
+{ 
+	// Your program begins with a call to main(). 
+	// Prints "Hello, World" to the terminal window. 
+	public static void main(String args[]) 
+	{ 
+		System.out.println("Hello, World"); 
+	} 
+} 
